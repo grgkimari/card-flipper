3f4654c0c91ce1be0f68741650650eead1cbf1a5
grgkimari <grgkimari@gmail.com>
2023-08-22 07:40:59 +0300

    Initial Commit

---
+++ CardFlipper/CardFlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlipperApp.swift
+//  CardFlipper
+//
+//  Created by George on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardFlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
