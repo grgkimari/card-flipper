1e715fe69235f462f7f9a9902d99d01f30b24e01
grgkimari <grgkimari@gmail.com>
2023-08-23 17:03:25 +0300

    Created CardView Struct

--- CardFlipper/ContentView.swift
+++ CardFlipper/ContentView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView : View{
+    var isFaceUp : Bool = false
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                Circle().strokeBorder(lineWidth: 2)
+                Circle().foregroundColor(.white)
+                Text("✈️").font(.largeTitle)
+            }
+            else{
+                Circle()
+            }
+            
+        }
     }
 }
