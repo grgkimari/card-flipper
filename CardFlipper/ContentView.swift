e5091659cd8ed55870040295a4a0cde081d11c9d
grgkimari <grgkimari@gmail.com>
2023-08-30 15:22:42 +0300

    Added buttons to add and remove cards

--- CardFlipper/ContentView.swift
+++ CardFlipper/ContentView.swift
@@ -8,19 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🚗","🚚","🚜","🚛","🚔"]
+    let emojis = ["🚗","🚚","🚜","🚛","🚔", "✈️", "🚀", "🚁", "🚤"]
+    @State var cardCount = 4
+    
     var body: some View {
+        VStack{
+            cards
+            cardControls        }
+    }
+    
+    //COMPONENTS
+    
+    func cardControl(action : String) -> some View {
+        if action == "REMOVE"{
+            return Button(action: {
+                    if cardCount > 2{
+                        cardCount -= 1}
+                }
+            , label:  {
+                Image(systemName: "minus.circle").imageScale(.large).font(.largeTitle)
+            })
+            
+        }
+        else {
+            return Button(action: {
+                
+                    if cardCount < emojis.count{
+                        cardCount += 1
+                        
+                    }
+                
+            }, label:  {
+                Image(systemName: "plus.circle").imageScale(.large).font(.largeTitle)
+            })
+        }
+    }
+    
+    var cards : some View {
         HStack{
-            ForEach(emojis.indices, id: \.self){ index in
+            ForEach(0..<cardCount, id: \.self){ index in
                 CardView(content : emojis[index])
                 
             }
             
-            
         }
-        
     }
-}
+    var cardRemover : some View {
+        cardControl(action: "REMOVE")
+            }
+    
+    var cardAdder : some View {
+        cardControl(action: "ADD")
+    }
+    var cardControls : some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }.padding()
+    }
+    }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
